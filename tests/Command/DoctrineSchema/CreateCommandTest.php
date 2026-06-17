3df5975fbebcd9043053f3ec662928174f86d487
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2026-06-17 22:48:16 +0300

    test: add DoctrineSchema command tests and regenerate phpstan baseline

---
+++ tests/Command/DoctrineSchema/CreateCommandTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) Precision Soft
+ */
+
+namespace PrecisionSoft\Doctrine\Audit\Test\Command\DoctrineSchema;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Mockery;
+use Mockery\MockInterface;
+use PrecisionSoft\Doctrine\Audit\Command\DoctrineSchema\CreateCommand;
+use PrecisionSoft\Doctrine\Audit\Contract\AnnotationReadServiceInterface;
+use PrecisionSoft\Symfony\Phpunit\MockDto;
+use PrecisionSoft\Symfony\Phpunit\TestCase\AbstractTestCase;
+use ReflectionMethod;
+use stdClass;
+
+/**
+ * @internal
+ */
+final class CreateCommandTest extends AbstractTestCase
+{
+    public static function getMockDto(): MockDto
+    {
+        return new MockDto(stdClass::class);
+    }
+
+    public function testGetSchemaSqlDelegatesToCreateSchemaSql(): void
+    {
+        $createCommand = $this->createCommand();
+
+        $metadatas = [];
+        $sqlStatements = ['CREATE TABLE audit_user'];
+
+        /** @var SchemaTool|MockInterface $schemaTool */
+        $schemaTool = Mockery::mock(SchemaTool::class);
+        $schemaTool->shouldReceive('getCreateSchemaSql')
+            ->once()
+            ->with($metadatas)
+            ->andReturn($sqlStatements);
+
+        $result = $this->invoke($createCommand, 'getSchemaSql', $schemaTool, $metadatas);
+
+        static::assertSame($sqlStatements, $result);
+    }
+
+    public function testExecuteSchemaDelegatesToCreateSchema(): void
+    {
+        $createCommand = $this->createCommand();
+
+        $metadatas = [];
+
+        /** @var SchemaTool|MockInterface $schemaTool */
+        $schemaTool = Mockery::mock(SchemaTool::class);
+        $schemaTool->shouldReceive('createSchema')
+            ->once()
+            ->with($metadatas);
+
+        $this->invoke($createCommand, 'executeSchema', $schemaTool, $metadatas);
+
+        static::assertSame('audit:doctrine:schema:create', $createCommand->getName());
+    }
+
+    public function testActionAndCompletedVerbs(): void
+    {
+        $createCommand = $this->createCommand();
+
+        static::assertSame('creating', $this->invoke($createCommand, 'getActionVerb'));
+        static::assertSame('created', $this->invoke($createCommand, 'getCompletedVerb'));
+    }
+
+    private function createCommand(): CreateCommand
+    {
+        return new CreateCommand(
+            'audit:doctrine:schema:create',
+            Mockery::mock(EntityManagerInterface::class),
+            Mockery::mock(EntityManagerInterface::class),
+            Mockery::mock(AnnotationReadServiceInterface::class),
+        );
+    }
+
+    private function invoke(CreateCommand $createCommand, string $method, mixed ...$arguments): mixed
+    {
+        $reflectionMethod = new ReflectionMethod($createCommand, $method);
+
+        return $reflectionMethod->invoke($createCommand, ...$arguments);
+    }
+}
---
+++ tests/Command/DoctrineSchema/UpdateCommandTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) Precision Soft
+ */
+
+namespace PrecisionSoft\Doctrine\Audit\Test\Command\DoctrineSchema;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Mockery;
+use Mockery\MockInterface;
+use PrecisionSoft\Doctrine\Audit\Command\DoctrineSchema\UpdateCommand;
+use PrecisionSoft\Doctrine\Audit\Contract\AnnotationReadServiceInterface;
+use PrecisionSoft\Symfony\Phpunit\MockDto;
+use PrecisionSoft\Symfony\Phpunit\TestCase\AbstractTestCase;
+use ReflectionMethod;
+use stdClass;
+
+/**
+ * @internal
+ */
+final class UpdateCommandTest extends AbstractTestCase
+{
+    public static function getMockDto(): MockDto
+    {
+        return new MockDto(stdClass::class);
+    }
+
+    public function testGetSchemaSqlDelegatesToUpdateSchemaSql(): void
+    {
+        $updateCommand = $this->createCommand();
+
+        $metadatas = [];
+        $sqlStatements = ['ALTER TABLE audit_user'];
+
+        /** @var SchemaTool|MockInterface $schemaTool */
+        $schemaTool = Mockery::mock(SchemaTool::class);
+        $schemaTool->shouldReceive('getUpdateSchemaSql')
+            ->once()
+            ->with($metadatas)
+            ->andReturn($sqlStatements);
+
+        $result = $this->invoke($updateCommand, 'getSchemaSql', $schemaTool, $metadatas);
+
+        static::assertSame($sqlStatements, $result);
+    }
+
+    public function testExecuteSchemaDelegatesToUpdateSchema(): void
+    {
+        $updateCommand = $this->createCommand();
+
+        $metadatas = [];
+
+        /** @var SchemaTool|MockInterface $schemaTool */
+        $schemaTool = Mockery::mock(SchemaTool::class);
+        $schemaTool->shouldReceive('updateSchema')
+            ->once()
+            ->with($metadatas);
+
+        $this->invoke($updateCommand, 'executeSchema', $schemaTool, $metadatas);
+
+        static::assertSame('audit:doctrine:schema:update', $updateCommand->getName());
+    }
+
+    public function testActionAndCompletedVerbs(): void
+    {
+        $updateCommand = $this->createCommand();
+
+        static::assertSame('updating', $this->invoke($updateCommand, 'getActionVerb'));
+        static::assertSame('updated', $this->invoke($updateCommand, 'getCompletedVerb'));
+    }
+
+    private function createCommand(): UpdateCommand
+    {
+        return new UpdateCommand(
+            'audit:doctrine:schema:update',
+            Mockery::mock(EntityManagerInterface::class),
+            Mockery::mock(EntityManagerInterface::class),
+            Mockery::mock(AnnotationReadServiceInterface::class),
+        );
+    }
+
+    private function invoke(UpdateCommand $updateCommand, string $method, mixed ...$arguments): mixed
+    {
+        $reflectionMethod = new ReflectionMethod($updateCommand, $method);
+
+        return $reflectionMethod->invoke($updateCommand, ...$arguments);
+    }
+}
